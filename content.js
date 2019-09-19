3429b8c9aa6c5faead4a30cef3baf525e5680c03
Hrvoje Matic <hrvoje.matic@productive.io>
2019-09-19 12:53:32 +0200

    Fix style

--- content.js
+++ content.js
@@ -1,25 +1,25 @@
-listings = document.querySelectorAll('.EntityList-item--Regular, .EntityList-item--VauVau');
+const listings = document.querySelectorAll('.EntityList-item--Regular, .EntityList-item--VauVau');
 
 for (var i = listings.length - 1; i >= 0; i--) {
-  listing = listings[i]
+  listing = listings[i];
 
   // Calculate sqm
-  var adDescription = listing.getElementsByClassName('entity-description-main')[0].textContent;
-  var sqm = parseFloat(adDescription.match(/Stambena površina: ([0-9.]+) m2/)[1]);
+  const adDescription = listing.getElementsByClassName('entity-description-main')[0].textContent;
+  const sqm = parseFloat(adDescription.match(/Stambena površina: ([0-9.]+) m2/)[1]);
 
   // Calculate price
-  var adPrice = listing.getElementsByClassName('price price--eur')[0].textContent;
-  var price = parseInt(adPrice.match(/[0-9.]+/)[0].replace(".", ""))
+  const adPrice = listing.getElementsByClassName('price price--eur')[0].textContent;
+  const price = parseInt(adPrice.match(/[0-9.]+/)[0].replace(".", ""));
 
-  var pricePerSqmCalculated = Math.round(price/sqm);
+  const pricePerSqmCalculated = Math.round(price/sqm);
 
   // Modify UI:
-  var priceList = listing.getElementsByClassName("price-items cf")[0];
+  let priceList = listing.getElementsByClassName("price-items cf")[0];
 
-  var pricePerSqm = document.createElement("li");
-  pricePerSqm.setAttribute("class", "price-item")
+  let pricePerSqm = document.createElement("li");
+  pricePerSqm.setAttribute("class", "price-item");
 
-  pricePerSqm.innerHTML = `<strong class="price">${pricePerSqmCalculated}</strong><span class="currency"> eur/m2</span>`
+  pricePerSqm.innerHTML = `<strong class="price">${pricePerSqmCalculated}</strong><span class="currency"> eur/m2</span>`;
 
   priceList.appendChild(pricePerSqm);
 }
